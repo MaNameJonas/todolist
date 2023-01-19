839bcf23174f7d4ef9df3da2c404d68932daee29
MaNameJonas <jonas.glieder@gmail.com>
2023-01-19 15:27:03 +0100

    Database Connection done + datamodel code

--- datamodel.py
+++ datamodel.py
@@ -1,5 +1,6 @@
 import sqlalchemy
 import sqlalchemy.ext.declarative
+from sqlalchemy.orm import relationship
 
 Base = sqlalchemy.ext.declarative.declarative_base()
 
@@ -11,13 +12,14 @@ class todo_item(Base):
     title = sqlalchemy.Column(sqlalchemy.String)
     description = sqlalchemy.Column(sqlalchemy.String)
     iscomplete = sqlalchemy.Column(sqlalchemy.Boolean)
-    # todo_list_id = relationship(todo_list)
+    todo_list_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("todo_list.todo_list_id"))
 
 
 class todo_list(Base):
     """Datamodel creation for Todo List Table (2/2)"""
 
     __tablename__ = "todo_list"
-    todo_list_id = sqlalchemy.Column(sqlalchemy.Integer)
+    todo_list_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     title = sqlalchemy.Column(sqlalchemy.String)
     colour = sqlalchemy.Column(sqlalchemy.String)
+    todo_item = relationship("todo_item")
